520e542b32a9f31226c509ef405aefbda95cf812
Igor Mariano <igor-mariano@outlook.com>
2023-03-08 20:27:37 -0300

    Criando for com c

---
+++ for.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%i", &n);
+    for(int i = 0; i < n;i++){
+        printf("Projeto de Software é a sua disciplina favorita!\n");
+    }
+    return 0;
+}
