9cf0ac75fd32717664333c39c324e248bd65d087
Igor Mariano <igor-mariano@outlook.com>
2023-04-04 12:39:33 -0300

    Enviando arquivo notas_prova_Array.c

---
+++ Exercícios/Array/notas_prova_Array.c
@@ -0,0 +1,18 @@
+// QUESTÃO 9)
+
+#include <stdio.h>
+int main(){
+    int tamanho_array;
+    float notas[100], soma = 0, media;
+    scanf("%i", &tamanho_array);
+
+    printf("Digite o total de notas que o sistema deve ler:")
+    scanf("%i", &tamanho_array)
+
+    for (int ){
+
+
+    }
+    
+    return 0;
+}
