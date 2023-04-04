e75e137407193570320b402e5b06c14953ad9cf8
Igor Mariano <igor-mariano@outlook.com>
2023-04-04 12:50:06 -0300

    Enviando arquivo Nota_Alunos.c

---
+++ Exercícios/Matriz/Exercícicos/Nota_Alunos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int soma = 0, media = 0, aprovado = 0, avaliacoes [6][4] = {{   10,	5,	1,	8,
+                                                                    8,	5,	7,	0,
+                                                                    6,	6,	5,	5,
+                                                                    8,	0,	7,	9,
+                                                                    6,	9,	5,	6,
+                                                                    9,	1,	6,	5
+                                                                }}
+
+    for (int alunos = 0; alunos < 6; alunos++)
+    {
+        for (int notas = 0; notas < 4; notas++)
+        {
+            if ()
+            {
+
+            }
+            
+        }
+        
+    }
+    return 0;
+}
