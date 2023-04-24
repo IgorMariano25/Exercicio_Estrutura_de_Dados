4b065cd7f2bff4cc993406ed4aa38cc52ded2d82
Igor Mariano <igor-mariano@outlook.com>
2023-04-24 19:54:06 -0300

    Enviando arquivo popular_Matriz.c

---
+++ Exercícios/SPOJ/Popular_Matriz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, matriz_1[n][n];
+    scanf("%i", n);
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (matriz[j][i] = )
+            {
+                
+            }
+        }
+    }
+
+    return 0;
+}
