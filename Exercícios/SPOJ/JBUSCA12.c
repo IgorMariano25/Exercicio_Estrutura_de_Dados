01d2ff4eb694cb005b2d62d59e0222e72a0dfb83
Igor Mariano <igor-mariano@outlook.com>
2023-04-24 19:53:47 -0300

    Enviando arquivo JBUSCA12.c

---
+++ Exercícios/SPOJ/JBUSCA12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int link_1, link_2, link_3;
+
+    scanf("%i", &link_3);
+    link_2 = 2 * link_3;
+    link_1 = link_2 * 2;
+    printf("%i", link_1);
+    return 0;
+}
