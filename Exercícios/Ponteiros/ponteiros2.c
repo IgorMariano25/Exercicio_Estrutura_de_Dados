a10da1f330f26ff4f7fa6abf4e99e2d3431a7071
Igor Mariano <igor-mariano@outlook.com>
2023-04-24 21:10:45 -0300

    Enviando arquivo ponteiros2.c

---
+++ Exercícios/Ponteiros/ponteiros2.c
@@ -0,0 +1,18 @@
+#include < stdio.h>
+
+int main()
+{
+
+    int x, y;
+    int ponteiroX, ponteiroY;
+
+    printf("Digite o valor de X: ");
+    scanf("%i", &x);
+    ponteiroX = x;
+
+    printf("Digite o valor de Y: ");
+    scanf("%i", &x);
+    ponteiroY = y;
+
+    return 0;
+}
