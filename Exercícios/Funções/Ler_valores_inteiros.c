c62ff65e7fff6bfc2190cdd0b440169491ecc062
Igor Mariano <igor-mariano@outlook.com>
2023-04-26 19:40:04 -0300

    Adicionando novas variáveis

--- Exercícios/Funções/Ler_valores_inteiros.c
+++ Exercícios/Funções/Ler_valores_inteiros.c
@@ -15,20 +15,25 @@ int quantidade_valores_arry(int array[], int tamanho)
     int quantidade_valores_lidos = 0;
     for (int i = 0; i < tamanho; i++)
     {
-        printf("%i ", array[i]);
+        quantidade_valores_lidos++;
     }
 }
 
 int main()
 {
     int array_inteiros[25], tamanho_arry_inteiros = 0;
+    int array_reais[25], tamanho_arry_reais = 0;
+    int opcao_escolhida;
     int valor_informado;
+
     while (valor_informado != -1)
     {
         printf("Digite o valor para adicionar no Array: ");
         scanf("%i", &valor_informado);
+
         if (valor_informado == -1)
         {
+            printf("\n");
             break;
         }
         else
