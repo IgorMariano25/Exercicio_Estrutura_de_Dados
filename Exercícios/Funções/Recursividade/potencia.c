287bf7f9cc778264d195c62b78a59c3e74ed275e
Igor Mariano <igor-mariano@outlook.com>
2023-05-03 18:08:36 -0300

    Enviando arquivo potencia.c utilizando recursividade

--- Exercícios/Funções/Recursividade/potencia.c
+++ Exercícios/Funções/Recursividade/potencia.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+int potencia(int numero)
+{
+    if (numero == 0)
+    {
+        return 1;
+    }
+    else if (numero == 1)
+    {
+        return 2;
+    }
+    else
+    {
+        return 2 * (potencia(numero - 1));
+    }
+}
 int main()
 {
+    int numero;
+    printf("Informe o valor da potencia: ");
+    scanf("%i", &numero);
 
+    printf("O resultado de 2 eleavado a potencia %i e: %i", numero, potencia(numero));
     return 0;
 }
