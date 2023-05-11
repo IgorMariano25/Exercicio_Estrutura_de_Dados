3f94d928e4a22dfc2d565660a177a008906cb4b6
Igor Mariano <igor-mariano@outlook.com>
2023-05-11 10:36:24 -0300

    Enviando arquivo definicao_pessoa.endereco.c

---
+++ Exercícios/Registro/definicao_pessoa_endereco.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef char String[255];
+
+struct registro_endereco
+{
+    String logradouro;
+    int numero;
+    String bairro;
+    String cidade;
+    String estado;
+};
+
+struct registro_pessoa
+{
+    String nome;
+    String cpf;
+    struct registro_endereco endereco;
+};
+
+int main()
+{
+
+    struct registro_pessoa teste_pessoa;
+    strcpy(teste_pessoa.cpf, "12345678900");
+    strcpy(teste_pessoa.nome, "Igor Mariano");
+
+    strcpy(teste_pessoa.endereco.logradouro, "Rua");
+    teste_pessoa.endereco.numero = 53;
+    strcpy(teste_pessoa.endereco.bairro, "Sao Francisco");
+    strcpy(teste_pessoa.endereco.cidade, "cidade A");
+    strcpy(teste_pessoa.endereco.estado, "estado B");
+
+    printf("Nome: %s\n", teste_pessoa.nome);
+    printf("CPF: %s\n", teste_pessoa.cpf);
+    printf("Logradoudo: %s\n", teste_pessoa.endereco.logradouro);
+    printf("Numero: %d\n", teste_pessoa.endereco.numero);
+    printf("bairro: %s\n", teste_pessoa.endereco.bairro);
+    printf("cidade: %s\n", teste_pessoa.endereco.cidade);
+    printf("estado: %s\n", teste_pessoa.endereco.estado);
+
+    return 0;
+}
